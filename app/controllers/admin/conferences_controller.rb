292840a071cdefe8b38e4b8341b32aeb447ea2e3
Alexander Borovsky <alex.borovsky@gmail.com>
2009-03-11 23:37:02 +0200

    Fixed conference editor

--- app/controllers/admin/conferences_controller.rb
+++ app/controllers/admin/conferences_controller.rb
@@ -3,8 +3,6 @@ module Admin
     before_filter :admin_required, :scaffold_action
 
     layout "admin"
-    before_filter :populate_params
-
     active_scaffold :conferences do |config|
       config.columns = [:name, :start_date, :finish_date, :registration_opened]
       config.columns[:registration_opened].form_ui = :checkbox
