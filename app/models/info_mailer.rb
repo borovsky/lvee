a7a2f45dc6c9aab10f831be6b67aeb0097e2e5ab
Alexander Borovsky <alex.borovsky@gmail.com>
2009-04-06 22:04:17 +0300

    Fixed link to license

--- app/models/info_mailer.rb
+++ app/models/info_mailer.rb
@@ -7,10 +7,10 @@ class InfoMailer < ActionMailer::Base
 
     @body[:text]  = text
 
-    
+
     attachment(:content_type => attachment.content_type,
-      :body => attachment.read, 
+      :body => attachment.read,
       :filename => attachment.original_filename) unless attachment.blank?
-    
+
   end
 end
