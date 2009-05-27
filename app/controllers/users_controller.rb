192e167171dc7aed3ec1f1524082aa8b29a25adf
Alexander Borovsky <aborovsky@exadel.com>
2009-05-27 13:13:35 +0300

    Fixed registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_filter :login_required, :only => [:current]
   before_filter :scaffold_action, :set_common_columns_info, :only => [:edit, :update, :new, :create]
   before_filter(:current_user_only, :unless => :admin?,
-    :except => [:restore, :activate, :current])
+    :except => [:restore, :activate, :current,:new, :create])
 
 
   USER_EDITABLE_COLUMNS = [:password, :password_confirmation, :email, :first_name, :last_name, :country, :city,
