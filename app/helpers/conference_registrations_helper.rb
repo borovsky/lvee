6c79d28af68a052ace92b1bed2f19b7bfd9a6f43
Alexander Borovsky <alex.borovsky@gmail.com>
2009-06-06 12:28:29 +0300

    Added conference appartment questions

--- app/helpers/conference_registrations_helper.rb
+++ app/helpers/conference_registrations_helper.rb
@@ -9,4 +9,41 @@ module ConferenceRegistrationsHelper
     end
     values = [[t("label.common._select_"), ""]] + values
   end
+
+  def floor_form_column(record, input_name)
+    content_tag("div", check_box("record", "floor"))
+  end
+
+  def days_form_column(record, input_name)
+    translated_days = I18n.translate(:'date.day_names')
+    end_days = I18n.translate(:'date.day_names', :locale => I18n.default_locale)
+    days = translated_days.zip(end_days)
+    selectable_days = days[4..-1] + [days[0]]
+    selected_days = @record.days
+
+    days_list = selectable_days.map do |day|
+      content_tag(:li,  check_box_tag("record[days][]", day[1], selected_days.include?(day[1]))+  h(day[0]))
+    end
+    content_tag(:div, content_tag(:ul, days_list.join("\n")) +
+      hidden_field_tag("record[days][]", ""))
+  end
+
+  def tshirt_form_column(record, input_name)
+    s = ""
+    selected_sizes = @record.tshirt
+    p selected_sizes
+    @record.quantity.times do|i|
+      s << select_tag("record[tshirt][]", options_for_select(TSHIRT_SIZES, selected_sizes[i]))
+    end
+    content_tag(:div, s)
+  end
+
+  def transport_from_form_column(record, input_name)
+    select("record", "transport_from",  transport_options(:from))
+  end
+
+  def transport_to_form_column(record, input_name)
+    select("record", "transport_to",  transport_options(:to))
+  end
+
 end
