1b039a371c8079a2a47adaf7ba80e86c7385649b
Alexander Borovsky <alex.borovsky@gmail.com>
2009-07-01 02:09:19 +0300

    Fixed statistics page

--- vendor/gems/garb-0.2.3/lib/garb/profile.rb
+++ vendor/gems/garb-0.2.3/lib/garb/profile.rb
@@ -1,27 +1,27 @@
 module Garb
   class Profile
-    
+
     attr_reader :table_id, :title, :account_name
-    
+
     class Property
       include HappyMapper
-      
+
       tag 'property'
       namespace 'dxp'
-      
+
       attribute :name, String
       attribute :value, String
     end
 
     class Entry
       include HappyMapper
-      
+
       tag 'entry'
-      
+
       element :id, Integer
       element :title, String
       element :tableId, String, :namespace => 'dxp'
-      
+
       # has_one :table_id, TableId
       has_many :properties, Property
     end
@@ -31,14 +31,14 @@ module Garb
       @table_id = entry.tableId
       @account_name = entry.properties.detect{|p| p.name == 'ga:accountName'}.value
     end
-    
+
     def id
       @table_id.sub(/^ga:/, '')
     end
-    
+
     def self.all
-      url = "https://www.google.com/analytics/feeds/accounts/#{Session.email}"
-      response = DataRequest.new(url).send_request      
+      url = "https://www.google.com/analytics/feeds/accounts/default"
+      response = DataRequest.new(url).send_request
       Entry.parse(response.body).map {|e| Garb::Profile.new(e)}
     end
   end
