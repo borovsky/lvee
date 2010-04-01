bd1afa9f0a517a12f29deb413e57ee60e64c50b5
Alexander Borovsky <aborovsky@exadel.com>
2010-04-01 11:40:01 +0300

    Fixed error after profile save

--- vendor/plugins/active_scaffold/lib/active_scaffold/actions/core.rb
+++ vendor/plugins/active_scaffold/lib/active_scaffold/actions/core.rb
@@ -89,7 +89,7 @@ module ActiveScaffold::Actions
 
     # Redirect to the main page (override if the ActiveScaffold is used as a component on another controllers page) for Javascript degradation
     def return_to_main
-      redirect_to main_path_to_return
+      redirect_to main_path_to_return unless performed?
     end
 
     # Override this method on your controller to define conditions to be used when querying a recordset (e.g. for List). The return of this method should be any format compatible with the :conditions clause of ActiveRecord::Base's find.
