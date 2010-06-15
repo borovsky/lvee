61d28d296a82377c2b62bdfcb11409e1b7dd55e0
Alexander Borovsky <aborovsky@exadel.com>
2010-06-15 20:30:01 +0300

    Removed CSV export for badges Removed format for PDF export

--- app/controllers/admin/conferences_controller.rb
+++ app/controllers/admin/conferences_controller.rb
@@ -8,17 +8,13 @@ module Admin
     USER_CSV_COLUMNS = [:login, :full_name, :email, :country, :city, :occupation, :projects]
     REGISTRATION_CSV_COLUMNS = [:status_name, :comment, :proposition, :quantity, :days, :meeting, :phone, :residence, :floor, :transport_to, :transport_from, :food, :tshirt]
 
-    BADGE_USER_COLUMNS = [:login, :full_name, :from]
-    BADGE_COLUMNS = [:tags, :top, :bottom]
-
     layout "admin"
     active_scaffold :conferences do
       self.columns = [:name, :start_date, :finish_date, :registration_opened]
       self.columns[:registration_opened].form_ui = :checkbox
       self.action_links.add(:registrations, :label => :registrations, :type => :member, :inline => false, :parameters => {})
       self.action_links.add(:csv, :label => :csv_export, :type => :member, :inline => false, :parameters => {:format =>"csv" })
-      self.action_links.add(:badges_csv, :label => :badges_csv_export, :type => :member, :inline => false, :parameters => {:format =>"csv" })
-      self.action_links.add(:badges_pdf, :label => :badges_pdf_export, :type => :member, :inline => false, :parameters => {:format =>"pdf" })
+      self.action_links.add(:badges_pdf, :label => :badges_pdf_export, :type => :member, :inline => false)
     end
 
     def csv
@@ -33,27 +29,6 @@ module Admin
           row += REGISTRATION_CSV_COLUMNS.map {|c| reg.send(c) }
           row.map!{ |i| i.to_s.gsub("\n", " ").gsub("\r", " ")}
           csv << row
-        end
-      end
-      render :text => out, :content_type => "text/csv"
-    end
-
-    def badges_csv
-      out = FasterCSV.generate do |csv|
-        header = BADGE_USER_COLUMNS.map {|c| t("label.user.#{c}") }
-        header += BADGE_COLUMNS.map {|c| t("label.badge.#{c}") }
-        csv << header
-
-        conf = Conference.find(params[:id])
-        badges = conf.badges
-        badges.each do |badge|
-          user = badge.conference_registration.user
-          if badge.conference_registration.approved?
-            row = BADGE_USER_COLUMNS.map {|c| user.send(c) }
-            row += BADGE_COLUMNS.map {|c| badge.send(c) }
-            row.map!{ |i| i.to_s.gsub("\n", " ").gsub("\r", " ")}
-            csv << row
-          end
         end
       end
       render :text => out, :content_type => "text/csv"
