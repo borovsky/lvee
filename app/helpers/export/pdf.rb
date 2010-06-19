826a0b317cdd73acfc40e53d159b62f1eaf67f4e
Alexander Borovsky <alex.borovsky@gmail.com>
2010-06-19 19:00:21 +0300

    Fixed badges size

--- app/helpers/export/pdf.rb
+++ app/helpers/export/pdf.rb
@@ -8,19 +8,19 @@ module Export
       cell(w, 0, txt, 0, 0, align)
     end
 
-    def GetWPt
+    def w_pt
       @w_pt
     end
 
-    def GetHPt
+    def h_pt
       @h_pt
     end
 
-    def getLMargin
+    def l_margin
       @l_margin
     end
 
-    def getTMargin
+    def t_margin
       @t_margin
     end
   end
@@ -32,35 +32,36 @@ module Export
 
     def badges_export(badges)
       pdf = MFPDF.new("P", "pt")
-      pdf.SetFont("FreeSans")
-      pdf.SetTitle("")
-      pdf.SetAutoPageBreak(false)
+      pdf.set_font("FreeSans")
+      pdf.set_title("")
+      pdf.set_auto_page_break(false)
+      pdf.set_margins((72/25.4) * 15, (72/25.4) * 10)
 
-      height = (pdf.GetHPt - 2 * pdf.getLMargin) / BADGES_PER_COLUMN
-      width = (pdf.GetWPt -  2 * pdf.getTMargin) / BADGES_PER_ROW
+      height = (pdf.h_pt - 2 * pdf.t_margin) / BADGES_PER_COLUMN
+      width = (pdf.w_pt -  2 * pdf.l_margin) / BADGES_PER_ROW
 
-      start_pos_x = pdf.getLMargin
-      start_pos_y = pdf.getTMargin
+      start_pos_x = pdf.l_margin
+      start_pos_y = pdf.t_margin
 
-      pdf.SetDrawColor(0,0,0)
+      pdf.set_draw_color(0,0,0)
 
       badges.each_with_index do |b, idx|
         if(idx % (BADGES_PER_ROW * BADGES_PER_COLUMN) == 0)
-          pdf.AddPage
+          pdf.add_page
         end
 
         sx = start_pos_x + width * (idx % BADGES_PER_ROW)
         sy = start_pos_y + height * ((idx / BADGES_PER_ROW) % BADGES_PER_COLUMN)
 
-        pdf.Rect(sx, sy, width, height)
-        pdf.Image("#{RAILS_ROOT}/media/shishky.png", sx+6, sy + 10, 90)
-        pdf.Image("#{RAILS_ROOT}/media/logo2010.png", sx + 110, sy + 30, 140)
+        pdf.rect(sx, sy, width, height)
+        pdf.image("#{RAILS_ROOT}/media/shishky.png", sx+6, sy + 10, 90)
+        pdf.image("#{RAILS_ROOT}/media/logo2010.png", sx + 110, sy + 30, 140)
         pdf.add_text_wrap(sx + 100, sy + 10, width-110, b.tags, 12, 'C')
         pdf.add_text_wrap(sx + 20, sy + height - 50, width-40, b.top, 15, 'C')
         pdf.add_text_wrap(sx + 20, sy + height - 20, width-40, b.bottom, 12, 'C')
       end
 
-      pdf.Output
+      pdf.output
     end
 
   end
