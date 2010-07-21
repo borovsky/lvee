23f2d732225af177b28f3352e2d8da4413df920c
Alexander Borovsky <aborovsky@exadel.com>
2010-07-21 18:10:06 +0300

    Fixed from address for errors

--- config/initializers/error_handle.rb
+++ config/initializers/error_handle.rb
@@ -1,5 +1,5 @@
 ExceptionNotifier.exception_recipients = %w(alex.borovsky@gmail.com)
-ExceptionNotifier.sender_address = %("Error reported" <app.error@rcee.org>)
+ExceptionNotifier.sender_address = %("Error reported" <app.error@lvee.org>)
 
 # defaults to "[ERROR] "
 ExceptionNotifier.email_prefix = "[LVEE-ERROR] "
