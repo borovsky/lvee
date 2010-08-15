6d28446e2b952eec8a475f85749175cc4f77e8cf
Alexander Borovsky <alex.borovsky@gmail.com>
2010-08-15 16:39:20 +0300

    Removed old javascripts/styles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,33 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+(function() {
+    var timeout         = 500;
+    var closetimer		= 0;
+    var ddmenuitem      = 0;
+
+    function jsddm_open() {
+        jsddm_canceltimer();
+        jsddm_close();
+        ddmenuitem = $(this).find('ul').eq(0).css('visibility', 'visible');
+    }
+
+    function jsddm_close() {
+        if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');
+    }
+
+    function jsddm_timer() {
+        closetimer = window.setTimeout(jsddm_close, timeout);
+    }
+
+    function jsddm_canceltimer() {
+        if(closetimer) {
+            window.clearTimeout(closetimer);
+            closetimer = null;
+        }
+    }
+
+    $(document).ready(function() {
+        $('#jsddm > li').bind('mouseover', jsddm_open);
+        $('#jsddm > li').bind('mouseout',  jsddm_timer);
+    });
+
+    document.onclick = jsddm_close;
+})();
