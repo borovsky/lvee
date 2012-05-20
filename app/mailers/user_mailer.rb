f443c517a8a308000e936fc991d56cbab656e1b9
Alexander Borovsky <alex.borovsky@gmail.com>
2012-05-20 07:45:42 +0300

    Fixed link to activate account on summer site

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,13 @@ class UserMailer < ActionMailer::Base
 
   def signup_notification(user)
     @user = user
-    @url = "http://winter.lvee.org/activate/#{user.activation_code}"
+    @url = "http://summer.lvee.org/activate/#{user.activation_code}"
     mail :to => user.email, :subject => PREFIX + I18n.t('mail.subject.activation')
   end
 
   def activation_restore(user)
     @user = user
-    @url = "http://winter.lvee.org/activate/#{user.activation_code}"
+    @url = "http://summer.lvee.org/activate/#{user.activation_code}"
 
     mail :to => user.email, :subject => PREFIX + I18n.t('mail.subject.activation_restore')
   end
@@ -18,14 +18,14 @@ class UserMailer < ActionMailer::Base
 
   def activation(user)
     @user = user
-    @url = "http://winter.lvee.org/"
+    @url = "http://summer.lvee.org/"
 
     mail :to => user.email, :subject => PREFIX + I18n.t('mail.subject.activation_complete')
   end
 
   def password_restore(user, ip)
     @user = user
-    @url = "http://winter.lvee.org/"
+    @url = "http://summer.lvee.org/"
     @ip = ip
 
     mail :to => user.email, :subject => PREFIX + I18n.t('mail.subject.password_restore')
