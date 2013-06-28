2dcc2da38e633bb8e76eaa468dc3718709192998
Alexander Borovsky <alex.borovsky@gmail.com>
2013-06-28 09:39:06 +0300

    Updated ruby version in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :user, 'partizan'
 # Server env
 set :using_rvm, true
 set :rvm_type, :user
-set :rvm_ruby_string, 'ruby-1.9.3-p0'
+set :rvm_ruby_string, 'ruby-1.9.3-p429'
 
 set :use_sudo, false
 
