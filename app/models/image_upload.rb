76c99a2402c07337a406539c36eb5e91d81bf4bc
fiowro <d.k@list.ru>
2014-06-10 20:20:25 +0300

    Update image_upload.rb
    A small fix to make avatars uploading again. Perhaps there where changes in rMagick or so...

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -5,7 +5,7 @@ class ImageUpload < ActiveRecord::Base
 
   def set_image_size
     if self.image.current_path
-      imgs = Magick::Image.read(self.image.current_path)
+      imgs = ImageMagick::Image.read(self.image.current_path)
       if(imgs.length > 0)
         img = imgs.first
         self.width = img.columns
