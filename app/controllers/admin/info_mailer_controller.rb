lvee/fantasy-engine

Removed attachments to emails

app/controllers/admin/info_mailer_controller.rb
@@ -13,13 +13,13 @@ class Admin::InfoMailerController < ApplicationController
 
   def send_mail
     if @to_list_users.empty?
-      InfoMailer.info_mail(params[:to], params[:subject], params[:body], params[:attachment]).deliver
+      InfoMailer.info_mail(params[:to], params[:subject], params[:body]).deliver
     else
       @to_list_users.each do |user|
         body = params[:body].gsub(/\{\{user\}\}/, user.full_name)
 
 
-        InfoMailer.info_mail(user.email, params[:subject], body, params[:attachment]).deliver
+        InfoMailer.info_mail(user.email, params[:subject], body).deliver
       end
     end
 

borovsky/lvee

app/controllers/admin/info_mailer_controller.rb
@@ -13,13 +13,13 @@ class Admin::InfoMailerController < ApplicationController
 
   def send_mail
     if @to_list_users.empty?
-      InfoMailer.info_mail(params[:to], params[:subject], params[:body], params[:attachment]).deliver_now
+      InfoMailer.info_mail(params[:to], params[:subject], params[:body]).deliver_now
     else
       @to_list_users.each do |user|
         body = params[:body].gsub(/\{\{user\}\}/, user.full_name)
 
 
-        InfoMailer.info_mail(user.email, params[:subject], body, params[:attachment]).deliver_now
+        InfoMailer.info_mail(user.email, params[:subject], body).deliver_now
       end
     end
 
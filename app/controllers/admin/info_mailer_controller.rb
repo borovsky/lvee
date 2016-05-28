6cb727e3c11016578d5b811abfd6ae902ebdc053
Alexandr Dubitsky <alexandr.dubitsky@gmail.com>
2016-05-28 11:08:02 +0300

    Removed attachments to emails

--- app/controllers/admin/info_mailer_controller.rb
+++ app/controllers/admin/info_mailer_controller.rb
@@ -13,13 +13,13 @@ class Admin::InfoMailerController < ApplicationController
 
   def send_mail
     if @to_list_users.empty?
-      InfoMailer.info_mail(params[:to], params[:subject], params[:body], params[:attachment]).deliver_now
+      InfoMailer.info_mail(params[:to], params[:subject], params[:body]).deliver_now
     else
       @to_list_users.each do |user|
         body = params[:body].gsub(/\{\{user\}\}/, user.full_name)
 
 
-        InfoMailer.info_mail(user.email, params[:subject], body, params[:attachment]).deliver_now
+        InfoMailer.info_mail(user.email, params[:subject], body).deliver_now
       end
     end
 
--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -1,12 +1,7 @@
 class InfoMailer < ActionMailer::Base
   default :from => "info@lvee.org"
 
-  def info_mail(to, subject, text, attachment)
-    
-    attachment(:content_type => attachment.content_type,
-      :body => attachment.read,
-      :filename => attachment.original_filename) unless attachment.blank?
-
+  def info_mail(to, subject, text)
     mail(:to => to, :subject => subject, :body => text)
   end
 end
