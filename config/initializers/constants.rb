9a76064b510586d0e94aa4ebe6313fca564b5482
Alexandr D <alex-liberty@users.noreply.github.com>
2017-02-21 00:58:01 +0300

    Add variables of environment

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -50,10 +50,10 @@ DEFAULT_LICENSE = "Creative Commons Attribution-ShareAlike 3.0"
 
 LATEX_WS_BASE = 'http://www.codecogs.com/png.latex?'
 
-SITE_DOMAIN = "lvee.org"
-SITE_PROTOCOL = "https"
+SITE_DOMAIN = ENV['DOMAIN'] || "lvee.org"
+SITE_PROTOCOL = ENV['PROTOCOL'] || "https"
 SITE_URL = "#{SITE_PROTOCOL}://#{SITE_DOMAIN}/"
-SITE_TITLE = "LVEE"
-SITE_FULL_TITLE = "Linux Vacation/Eastern Europe"
-INFO_MAIL = "info@lvee.org"
-CHROME_TAB_COLOR = "#94CD33"
+SITE_TITLE = ENV['TITLE'] || "LVEE"
+SITE_FULL_TITLE = ENV['FULL_TITLE'] || "Linux Vacation/Eastern Europe"
+INFO_MAIL = ENV['MAIL'] || "info@lvee.org"
+CHROME_TAB_COLOR = ENV['TAB_COLOR'] || "#94CD33"
